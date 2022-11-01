3d6a5a25391526002bbe07d38c5eadd71210ba62
Sam <contact@codeyverse.com>
2022-11-01 20:53:25 +1100

    Changed A to Link

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,9 +1,13 @@
+import {Link} from "react-router-dom";
+
 const Nav = () => {
   return (
     <nav className="flex bg-slate-700 py-2 px-4 items-center">
-      <a className="text-4xl text-white ml-auto mr-auto font-Jolly-Lodger" href="/">
+      <Link to="/">
+      <h1 className="text-4xl text-white ml-auto mr-auto font-Jolly-Lodger" href="/">
         404 Pages
-      </a>
+      </>
+      </Link>
       <a href="https://github.com/ahmed-sudani/404-pages">
         <img src="/GitHub-Mark-Light-32px.png" alt="404-pages" />
       </a>

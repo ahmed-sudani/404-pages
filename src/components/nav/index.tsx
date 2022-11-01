7f6ac41775ae540c20d64778b79d8e81b36b6772
Sam <contact@codeyverse.com>
2022-11-01 20:28:26 +1100

    Make title link on nav

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,9 +1,9 @@
 const Nav = () => {
   return (
     <nav className="flex bg-slate-700 py-2 px-4 items-center">
-      <h1 className="text-4xl text-white ml-auto mr-auto font-Jolly-Lodger">
+      <a className="text-4xl text-white ml-auto mr-auto font-Jolly-Lodger" href="/">
         404 Pages
-      </h1>
+      </a>
       <a href="https://github.com/ahmed-sudani/404-pages">
         <img src="/GitHub-Mark-Light-32px.png" alt="404-pages" />
       </a>

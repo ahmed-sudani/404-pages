72b776f1a1c82ac11634f934d9ac131ba1811651
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-11-02 22:00:20 +0200

    space animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,26 @@ module.exports = {
           '0%': { 'border-color': 'transparent' },
           '50%': { 'border-color': 'gray' },
         },
+        spaceMove: {
+          '0%': { 'background-position': 'top , center' },
+          '100%': { 'background-position': '250% 250% , center' },
+        },
+        textViewNormal: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       },
       animation: {
         typing:
           'typing 3.5s steps(40, end) infinite, blink 0.75s step-end infinite',
+        spaceMove: 'spaceMove 4s infinite',
+        textViewNormal: 'textViewNormal 4s infinite',
+      },
+      backgroundPosition: {
+        space: '192% 192%, center',
+      },
+      backgroundImage: {
+        space: 'url("../public/Space1.png"), url(../public/Space2.png)',
       },
     },
   },
